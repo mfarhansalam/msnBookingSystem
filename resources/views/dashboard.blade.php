0f132b18fe27587971a8c4fa179a9ce5470b6490
mfarhansalam <muhdfarhan7780@gmail.com>
2021-12-28 23:08:32 +0800

    day 4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,8 +20,8 @@
                   <div class="btn-group">
                     
                    <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                     Tempah
+                    <button  type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal{{($court->id) }}">
+                     Booking Here
                     </button>
                   </div>
                   <small class="text-muted">{{$court->price}}/JAM</small>
@@ -33,28 +33,37 @@
       </div>
      
     </div>
-    @endforeach
-    <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
-      </button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal{{($court->id) }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Booking Form</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p class="display-5">{{$court->name}}</p>
+            <input type="hidden" id="courtname" name="courtname" value="{{$court->name}}">
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="floatingInput" name="email" value="{{ old('email') ?? auth()->user()->email }}" required autofocus />
+             
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+              <input type="date" class="form-control" id="floatingDate"  name="datebook">
+              
+              <label for="floatingDate">Date</label>
+            </div><br>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save </button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+    @endforeach
+   
+
+    
 @endsection
